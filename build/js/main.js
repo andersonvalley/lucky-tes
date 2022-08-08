c57569453a6075504d1460a8df93196343311f13
Andrey Mironov <andrey@MacBook-Pro-Andrey.local>
2022-08-09 01:53:28 +0300

    safari поиск исправлен

--- build/js/main.js
+++ build/js/main.js
@@ -1,1 +1,255 @@
-(()=>{"use strict";const e=e=>{const t=document.querySelector(".hero__descr");e||(t.innerHTML="Ошибка загрузки, попробуйте еще раз"),t.innerHTML=e[0]};!async function(){try{const t=await fetch("https://baconipsum.com/api/?type=lucky"),n=await t.json();e(n)}catch(t){console.log(t),e()}}(),document.addEventListener("DOMContentLoaded",(()=>{(()=>{const e=document.querySelector(".animation__list");[{imgUrl:"img/flag1.svg",name:"flag"},{imgUrl:"img/flag3.svg",name:"flag"},{imgUrl:"img/flag2.svg",name:"flag"},{imgUrl:"img/flag1.svg",name:"flag"},{imgUrl:"img/flag1.svg",name:"flag"}].forEach((t=>{const n='\n      <li class="animation__item animation__item-hidden">\n        <img src="'.concat(t.imgUrl,'" alt="').concat(t.name,'" />\n      </li>\n    ');e.insertAdjacentHTML("beforeend",n)}));let t=3e3;Array.from(e.children).forEach((e=>{t+=400,setTimeout((()=>{e.classList.remove("animation__item-hidden"),setTimeout((()=>{document.querySelector(".animation__list").style.animation="2s ease-in-out infinite rot"}),t-3e3)}),t)}))})(),(()=>{const e=document.forms.search,t=document.querySelector(".error");e&&(function(e,t){const n=/[!@#$%^&*()]/g;e.addEventListener("input",(e=>{e.target.value=e.target.value.replace(n,""),e.target.value.length>=4&&(t.innerHTML="")}))}(e.elements.searchInput,t),e.addEventListener("submit",(n=>{n.preventDefault();const{searchInput:a}=Object.fromEntries(new FormData(n.target).entries());a.length<4||a.length>12?t.innerHTML="Минимальная длина: 4, максимальная: 12":e.reset()})))})(),function(){const e=document.querySelector(".burger"),t=document.querySelector(".header__container");e&&e.addEventListener("click",(()=>{if(e.classList.contains("burger--active"))return e.classList.remove("burger--active"),t.classList.remove("header-mobile"),void(document.body.style.overflow="");e.classList.add("burger--active"),t.classList.add("header-mobile"),document.body.style.overflow="hidden"}))}()}))})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/components/animate.js":
+/*!**************************************!*\
+  !*** ./src/js/components/animate.js ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "animate": () => (/* binding */ animate)
+/* harmony export */ });
+const animate = () => {
+  const animationList = document.querySelector('.animation__list');
+  const data = [{
+    imgUrl: 'img/flag1.svg',
+    name: 'flag'
+  }, {
+    imgUrl: 'img/flag3.svg',
+    name: 'flag'
+  }, {
+    imgUrl: 'img/flag2.svg',
+    name: 'flag'
+  }, {
+    imgUrl: 'img/flag1.svg',
+    name: 'flag'
+  }, {
+    imgUrl: 'img/flag1.svg',
+    name: 'flag'
+  }];
+  data.forEach(item => {
+    const url = "\n      <li class=\"animation__item animation__item-hidden\">\n        <img src=\"".concat(item.imgUrl, "\" alt=\"").concat(item.name, "\" />\n      </li>\n    ");
+    animationList.insertAdjacentHTML('beforeend', url);
+  });
+  let ms = 3000;
+  Array.from(animationList.children).forEach(item => {
+    ms += 400;
+    setTimeout(() => {
+      item.classList.remove('animation__item-hidden');
+      setTimeout(() => {
+        animateBy();
+      }, ms - 3000);
+    }, ms);
+  });
+};
+
+function animateBy() {
+  const animationlist = document.querySelector('.animation__list');
+  animationlist.style.animation = '2s ease-in-out infinite rot';
+}
+
+/***/ }),
+
+/***/ "./src/js/components/burger.js":
+/*!*************************************!*\
+  !*** ./src/js/components/burger.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "openMenu": () => (/* binding */ openMenu)
+/* harmony export */ });
+function openMenu() {
+  const burger = document.querySelector('.burger');
+  const headerContainer = document.querySelector('.header__container');
+
+  function showMenu() {
+    burger.classList.add('burger--active');
+    headerContainer.classList.add('header-mobile');
+    document.body.style.overflow = 'hidden';
+  }
+
+  function closeMenu() {
+    burger.classList.remove('burger--active');
+    headerContainer.classList.remove('header-mobile');
+    document.body.style.overflow = '';
+  }
+
+  if (burger) {
+    burger.addEventListener('click', () => {
+      if (burger.classList.contains('burger--active')) {
+        closeMenu();
+        return;
+      }
+
+      showMenu();
+    });
+  }
+}
+
+/***/ }),
+
+/***/ "./src/js/components/fetchData.js":
+/*!****************************************!*\
+  !*** ./src/js/components/fetchData.js ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "fetchData": () => (/* binding */ fetchData)
+/* harmony export */ });
+async function fetchData() {
+  const url = 'https://baconipsum.com/api/?type=lucky';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    showData(data);
+  } catch (error) {
+    console.log(error);
+    showData();
+  }
+}
+
+const showData = data => {
+  const descr = document.querySelector('.hero__descr');
+
+  if (!data) {
+    descr.innerHTML = 'Ошибка загрузки, попробуйте еще раз';
+  }
+
+  descr.innerHTML = data[0];
+};
+
+/***/ }),
+
+/***/ "./src/js/components/formSubmit.js":
+/*!*****************************************!*\
+  !*** ./src/js/components/formSubmit.js ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "formSubmit": () => (/* binding */ formSubmit)
+/* harmony export */ });
+const formSubmit = () => {
+  const form = document.forms.search;
+  const error = document.querySelector('.error');
+  if (!form) return;
+  validateInput(form.elements.searchInput, error);
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    const {
+      searchInput
+    } = Object.fromEntries(new FormData(e.target).entries());
+
+    if (searchInput.length < 4 || searchInput.length > 12) {
+      error.innerHTML = 'Минимальная длина: 4, максимальная: 12';
+      return;
+    }
+
+    form.reset();
+  });
+};
+
+function validateInput(input, error) {
+  const regex = /[!@#$%^&*()]/g;
+  input.addEventListener('input', e => {
+    e.target.value = e.target.value.replace(regex, '');
+
+    if (e.target.value.length >= 4) {
+      error.innerHTML = '';
+    }
+  });
+}
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _components_animate__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/animate */ "./src/js/components/animate.js");
+/* harmony import */ var _components_burger__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/burger */ "./src/js/components/burger.js");
+/* harmony import */ var _components_fetchData__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./components/fetchData */ "./src/js/components/fetchData.js");
+/* harmony import */ var _components_formSubmit__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./components/formSubmit */ "./src/js/components/formSubmit.js");
+
+
+
+
+(0,_components_fetchData__WEBPACK_IMPORTED_MODULE_2__.fetchData)();
+document.addEventListener('DOMContentLoaded', () => {
+  (0,_components_animate__WEBPACK_IMPORTED_MODULE_0__.animate)();
+  (0,_components_formSubmit__WEBPACK_IMPORTED_MODULE_3__.formSubmit)();
+  (0,_components_burger__WEBPACK_IMPORTED_MODULE_1__.openMenu)();
+});
+})();
+
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
